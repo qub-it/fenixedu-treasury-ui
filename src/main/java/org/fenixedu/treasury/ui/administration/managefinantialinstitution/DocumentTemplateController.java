643de6ac5088ba3a8a98ac6da45c01422f68b47f
JNPA <jnpa@campus.ul.pt>
2015-05-06 18:43:41 +0100

    Reorganize the package of the document template controller

--- src/main/java/org/fenixedu/treasury/ui/administration/managefinantialinstitution/DocumentTemplateController.java
+++ src/main/java/org/fenixedu/treasury/ui/administration/managefinantialinstitution/DocumentTemplateController.java
@@ -24,7 +24,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with FenixEdu Treasury.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.fenixedu.treasury.ui.administration.manageFinantialInstitution;
+package org.fenixedu.treasury.ui.administration.managefinantialinstitution;
 
 import java.io.IOException;
 import java.net.URLEncoder;
