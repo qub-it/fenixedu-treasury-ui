f92d8743a13cb311c3b574a29b270d16bcc21e3b
Ricardo Pedro <ricardo@soferragens.pt>
2015-05-29 11:23:46 +0100

    PaymentCodePool domain corrections

--- src/main/java/org/fenixedu/treasury/domain/paymentcodes/pool/PaymentCodePool.java
+++ src/main/java/org/fenixedu/treasury/domain/paymentcodes/pool/PaymentCodePool.java
@@ -27,160 +27,157 @@
  */
 package org.fenixedu.treasury.domain.paymentcodes.pool;
 
+import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.apache.commons.lang.StringUtils;
 import org.fenixedu.bennu.core.domain.Bennu;
-import org.fenixedu.commons.i18n.LocalizedString;
+import org.fenixedu.treasury.domain.FinantialInstitution;
 import org.fenixedu.treasury.domain.exceptions.TreasuryDomainException;
-import org.fenixedu.treasury.domain.paymentcodes.pool.PaymentCodePool_Base;
-import org.fenixedu.treasury.util.LocalizedStringUtil;
 
 import pt.ist.fenixframework.Atomic;
 
 public class PaymentCodePool extends PaymentCodePool_Base {
 
-	protected PaymentCodePool() {
-		super();
+    protected PaymentCodePool() {
+        super();
 //		setBennu(Bennu.getInstance());
-	}
+    }
 
-	protected void init(final java.lang.String name,
-			final java.lang.Integer minPaymentCodes,
-			final java.lang.Integer maxPaymentCodes,
-			final java.math.BigDecimal minAmount,
-			final java.math.BigDecimal maxAmount, final java.lang.Boolean active) {
-		setName(name);
-		setMinPaymentCodes(minPaymentCodes);
-		setMaxPaymentCodes(maxPaymentCodes);
-		setMinAmount(minAmount);
-		setMaxAmount(maxAmount);
-		setActive(active);
-		checkRules();
-	}
+    protected void init(final java.lang.String name, final java.lang.Integer minPaymentCodes,
+            final java.lang.Integer maxPaymentCodes, final java.math.BigDecimal minAmount, final java.math.BigDecimal maxAmount,
+            final java.lang.Boolean active) {
+        setName(name);
+        setMinPaymentCodes(minPaymentCodes);
+        setMaxPaymentCodes(maxPaymentCodes);
+        setMinAmount(minAmount);
+        setMaxAmount(maxAmount);
+        setActive(active);
+        checkRules();
+    }
 
-	private void checkRules() {
-		//
-		// CHANGE_ME add more busines validations
-		//
+    private void checkRules() {
+        //
+        // CHANGE_ME add more busines validations
+        //
 
-		// CHANGE_ME In order to validate UNIQUE restrictions
-		// if (findByName(getName().count()>1)
-		// {
-		// throw new
-		// TreasuryDomainException("error.PaymentCodePool.name.duplicated");
-		// }
-		// if (findByMinPaymentCodes(getMinPaymentCodes().count()>1)
-		// {
-		// throw new
-		// TreasuryDomainException("error.PaymentCodePool.minPaymentCodes.duplicated");
-		// }
-		// if (findByMaxPaymentCodes(getMaxPaymentCodes().count()>1)
-		// {
-		// throw new
-		// TreasuryDomainException("error.PaymentCodePool.maxPaymentCodes.duplicated");
-		// }
-		// if (findByMinAmount(getMinAmount().count()>1)
-		// {
-		// throw new
-		// TreasuryDomainException("error.PaymentCodePool.minAmount.duplicated");
-		// }
-		// if (findByMaxAmount(getMaxAmount().count()>1)
-		// {
-		// throw new
-		// TreasuryDomainException("error.PaymentCodePool.maxAmount.duplicated");
-		// }
-		// if (findByActive(getActive().count()>1)
-		// {
-		// throw new
-		// TreasuryDomainException("error.PaymentCodePool.active.duplicated");
-		// }
-	}
+        // CHANGE_ME In order to validate UNIQUE restrictions
+        // if (findByName(getName().count()>1)
+        // {
+        // throw new
+        // TreasuryDomainException("error.PaymentCodePool.name.duplicated");
+        // }
+        // if (findByMinPaymentCodes(getMinPaymentCodes().count()>1)
+        // {
+        // throw new
+        // TreasuryDomainException("error.PaymentCodePool.minPaymentCodes.duplicated");
+        // }
+        // if (findByMaxPaymentCodes(getMaxPaymentCodes().count()>1)
+        // {
+        // throw new
+        // TreasuryDomainException("error.PaymentCodePool.maxPaymentCodes.duplicated");
+        // }
+        // if (findByMinAmount(getMinAmount().count()>1)
+        // {
+        // throw new
+        // TreasuryDomainException("error.PaymentCodePool.minAmount.duplicated");
+        // }
+        // if (findByMaxAmount(getMaxAmount().count()>1)
+        // {
+        // throw new
+        // TreasuryDomainException("error.PaymentCodePool.maxAmount.duplicated");
+        // }
+        // if (findByActive(getActive().count()>1)
+        // {
+        // throw new
+        // TreasuryDomainException("error.PaymentCodePool.active.duplicated");
+        // }
+    }
 
-	@Atomic
-	public void edit(final java.lang.String name,
-			final java.lang.Integer minPaymentCodes,
-			final java.lang.Integer maxPaymentCodes,
-			final java.math.BigDecimal minAmount,
-			final java.math.BigDecimal maxAmount, final java.lang.Boolean active) {
-		setName(name);
-		setMinPaymentCodes(minPaymentCodes);
-		setMaxPaymentCodes(maxPaymentCodes);
-		setMinAmount(minAmount);
-		setMaxAmount(maxAmount);
-		setActive(active);
-		checkRules();
-	}
+    @Atomic
+    public void edit(final java.lang.String name, final java.lang.Integer minPaymentCodes,
+            final java.lang.Integer maxPaymentCodes, final java.math.BigDecimal minAmount, final java.math.BigDecimal maxAmount,
+            final java.lang.Boolean active) {
+        setName(name);
+        setMinPaymentCodes(minPaymentCodes);
+        setMaxPaymentCodes(maxPaymentCodes);
+        setMinAmount(minAmount);
+        setMaxAmount(maxAmount);
+        setActive(active);
+        checkRules();
+    }
 
-	public boolean isDeletable() {
-		return true;
-	}
+    public boolean isDeletable() {
+        return true;
+    }
 
-	@Atomic
-	public void delete() {
-		if (!isDeletable()) {
-			throw new TreasuryDomainException(
-					"error.PaymentCodePool.cannot.delete");
-		}
+    @Atomic
+    public void delete() {
+        if (!isDeletable()) {
+            throw new TreasuryDomainException("error.PaymentCodePool.cannot.delete");
+        }
 
 //		setBennu(null);
 
-		deleteDomainObject();
-	}
+        deleteDomainObject();
+    }
 
-	@Atomic
-	public static PaymentCodePool create(final java.lang.String name,
-			final java.lang.Integer minPaymentCodes,
-			final java.lang.Integer maxPaymentCodes,
-			final java.math.BigDecimal minAmount,
-			final java.math.BigDecimal maxAmount, final java.lang.Boolean active) {
-		PaymentCodePool paymentCodePool = new PaymentCodePool();
-		paymentCodePool.init(name, minPaymentCodes, maxPaymentCodes, minAmount,
-				maxAmount, active);
-		return paymentCodePool;
-	}
+    @Atomic
+    public static PaymentCodePool create(final java.lang.String name, final java.lang.Integer minPaymentCodes,
+            final java.lang.Integer maxPaymentCodes, final java.math.BigDecimal minAmount, final java.math.BigDecimal maxAmount,
+            final java.lang.Boolean active) {
+        PaymentCodePool paymentCodePool = new PaymentCodePool();
+        paymentCodePool.init(name, minPaymentCodes, maxPaymentCodes, minAmount, maxAmount, active);
+        return paymentCodePool;
+    }
 
-	// @formatter: off
-	/************
-	 * SERVICES *
-	 ************/
-	// @formatter: on
+    // @formatter: off
+    /************
+     * SERVICES *
+     ************/
+    // @formatter: on
 
-//	public static Stream<PaymentCodePool> findAll() {
-//		return Bennu.getInstance().getPaymentCodePoolsSet().stream();
-//	}
-//
-//	public static Stream<PaymentCodePool> findByName(final java.lang.String name) {
-//		return findAll().filter(i -> name.equalsIgnoreCase(i.getName()));
-//	}
-//
-//	public static Stream<PaymentCodePool> findByMinPaymentCodes(
-//			final java.lang.Integer minPaymentCodes) {
-//		return findAll().filter(
-//				i -> minPaymentCodes.equals(i.getMinPaymentCodes()));
-//	}
-//
-//	public static Stream<PaymentCodePool> findByMaxPaymentCodes(
-//			final java.lang.Integer maxPaymentCodes) {
-//		return findAll().filter(
-//				i -> maxPaymentCodes.equals(i.getMaxPaymentCodes()));
-//	}
-//
-//	public static Stream<PaymentCodePool> findByMinAmount(
-//			final java.math.BigDecimal minAmount) {
-//		return findAll().filter(i -> minAmount.equals(i.getMinAmount()));
-//	}
-//
-//	public static Stream<PaymentCodePool> findByMaxAmount(
-//			final java.math.BigDecimal maxAmount) {
-//		return findAll().filter(i -> maxAmount.equals(i.getMaxAmount()));
-//	}
-//
-//	public static Stream<PaymentCodePool> findByActive(
-//			final java.lang.Boolean active) {
-//		return findAll().filter(i -> active.equals(i.getActive()));
-//	}
+    public static Stream<PaymentCodePool> findAll() {
+        Set<PaymentCodePool> codes = new HashSet<PaymentCodePool>();
+
+        return Bennu.getInstance().getFinantialInstitutionsSet().stream().map(x -> x.getPaymentCodePoolsSet())
+                .reduce(codes, (a, b) -> {
+                    a.addAll(b);
+                    return a;
+                }).stream();
+    }
+
+    public static Stream<PaymentCodePool> findByName(final java.lang.String name, final FinantialInstitution finantialInstitution) {
+        return findByFinantialInstitution(finantialInstitution).filter(i -> name.equalsIgnoreCase(i.getName()));
+    }
+
+    public static Stream<PaymentCodePool> findByMinPaymentCodes(final java.lang.Integer minPaymentCodes,
+            final FinantialInstitution finantialInstitution) {
+        return findByFinantialInstitution(finantialInstitution).filter(i -> minPaymentCodes.equals(i.getMinPaymentCodes()));
+    }
+
+    public static Stream<PaymentCodePool> findByMaxPaymentCodes(final java.lang.Integer maxPaymentCodes,
+            final FinantialInstitution finantialInstitution) {
+        return findByFinantialInstitution(finantialInstitution).filter(i -> maxPaymentCodes.equals(i.getMaxPaymentCodes()));
+    }
+
+    public static Stream<PaymentCodePool> findByMinAmount(final java.math.BigDecimal minAmount,
+            final FinantialInstitution finantialInstitution) {
+        return findByFinantialInstitution(finantialInstitution).filter(i -> minAmount.equals(i.getMinAmount()));
+    }
+
+    public static Stream<PaymentCodePool> findByMaxAmount(final java.math.BigDecimal maxAmount,
+            final FinantialInstitution finantialInstitution) {
+        return findAll().filter(i -> maxAmount.equals(i.getMaxAmount()));
+    }
+
+    public static Stream<PaymentCodePool> findByActive(final java.lang.Boolean active,
+            final FinantialInstitution finantialInstitution) {
+        return findAll().filter(i -> active.equals(i.getActive()));
+    }
+
+    public static Stream<PaymentCodePool> findByFinantialInstitution(final FinantialInstitution finantialInstitution) {
+        return findAll().filter(i -> i.getFinantialInstitution().equals(finantialInstitution));
+    }
 
 }
