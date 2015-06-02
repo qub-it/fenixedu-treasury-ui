4e6eaa4e7420c38e1a46e63c96516c68e7d2afe2
Anil Mamede <anil.mamede@gmail.com>
2015-06-02 15:55:58 +0100

    Revert TreasuryEvent.findAll in order to refine in AcademicTreasuryEvent

--- src/main/java/org/fenixedu/treasury/ui/document/manageinvoice/DebitEntryController.java
+++ src/main/java/org/fenixedu/treasury/ui/document/manageinvoice/DebitEntryController.java
@@ -176,7 +176,7 @@ public class DebitEntryController extends TreasuryBaseController {
         }
         this.setDebitEntryBean(bean, model);
 
-        model.addAttribute("DebitEntry_event_options", TreasuryEvent.findActiveBy(debtAccount).collect(Collectors.toList()));
+        model.addAttribute("DebitEntry_event_options", TreasuryEvent.findActiveBy(debtAccount).collect(Collectors.<TreasuryEvent> toList()));
 
         if (debitNote == null) {
             addInfoMessage(BundleUtil.getString(Constants.BUNDLE,
@@ -330,7 +330,7 @@ public class DebitEntryController extends TreasuryBaseController {
         if (debitEntry.getFinantialDocument() == null || debitEntry.getFinantialDocument().isPreparing()) {
             setDebitEntryBean(new DebitEntryBean(debitEntry), model);
             model.addAttribute("DebitEntry_event_options",
-                    TreasuryEvent.findActiveBy(debitEntry.getDebtAccount()).collect(Collectors.toList()));
+                    TreasuryEvent.findActiveBy(debitEntry.getDebtAccount()).collect(Collectors.<TreasuryEvent> toList()));
             setDebitEntry(debitEntry, model);
             return "treasury/document/manageinvoice/debitentry/update";
         } else {
