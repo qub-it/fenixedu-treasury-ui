9dfa8496f9518a97320a685d7295cfe701b5dd5d
Ricardo Pedro <ricardo@soferragens.pt>
2015-07-06 12:48:16 +0100

    Commented the stubs/WSDL for erp_integration

--- src/main/java/org/fenixedu/treasury/services/integration/erp/ERPExternalServiceImplementation/SIAGExternalService.java
+++ src/main/java/org/fenixedu/treasury/services/integration/erp/ERPExternalServiceImplementation/SIAGExternalService.java
@@ -1,29 +1,24 @@
 package org.fenixedu.treasury.services.integration.erp.ERPExternalServiceImplementation;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
 
-import org.fenixedu.treasury.domain.exceptions.TreasuryDomainException;
 import org.fenixedu.treasury.domain.integration.ERPConfiguration;
-import org.fenixedu.treasury.services.integration.ERPExternalService;
-import org.fenixedu.treasury.services.integration.ERPExternalServiceService;
 import org.fenixedu.treasury.services.integration.erp.IERPExternalService;
 import org.fenixedu.treasury.services.integration.erp.dto.DocumentsInformationInput;
 import org.fenixedu.treasury.services.integration.erp.dto.IntegrationStatusOutput;
 
 public class SIAGExternalService implements IERPExternalService {
 
-    ERPExternalService _internalService;
+//    ERPExternalService _internalService;
 
     public SIAGExternalService(ERPConfiguration erpIntegrationConfiguration) {
-        try {
-            _internalService =
-                    new ERPExternalServiceService(new URL(erpIntegrationConfiguration.getExternalURL()))
-                            .getERPExternalServicePort();
-        } catch (MalformedURLException e) {
-            throw new TreasuryDomainException("error.SIAGExternalService.error.creating.stub");
-        }
+//        try {
+//            _internalService =
+//                    new ERPExternalServiceService(new URL(erpIntegrationConfiguration.getExternalURL()))
+//                            .getERPExternalServicePort();
+//        } catch (MalformedURLException e) {
+//            throw new TreasuryDomainException("error.SIAGExternalService.error.creating.stub");
+//        }
     }
 
     @Override
